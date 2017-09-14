d616ba37ca82ca4dda687e77d60f81e17614c82e
byfyang <darkspherico@gmail.com>
2017-09-14 11:02:51 -0700

    added in scorllrevealjs

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
-import Home from '../index.html'
-import Styles from '../styles/main.css'
-import Styles2 from '../styles/main.scss'
+import Home from '../index.html';
+import Styles from '../styles/main.css';
+import ScrollReveal from 'scrollreveal';
 
-console.log("working")
+
+console.log("working");
+
+
+window.sr = ScrollReveal();
+sr.reveal('.fadeInLeftBig', {duration: 1000, origin: 'bottom'});
