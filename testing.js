940a4fa158b48d86e20c4b4681d3d16bf4814d88
byfyang <darkspherico@gmail.com>
2017-09-14 17:26:19 -0700

    add google maps

--- testing.js
+++ testing.js
@@ -3,3 +3,20 @@ console.log("working");
 
 window.sr = ScrollReveal();
 sr.reveal('.fadeInLeftBig', {duration: 1000, origin: 'bottom'});
+
+
+function initMap() {
+       var uluru = {lat: 37.777769, lng: -122.411307};
+       var map = new google.maps.Map(document.getElementById('map'), {
+         zoom: 16,
+         center: uluru
+       });
+       var marker = new google.maps.Marker({
+         position: uluru,
+         map: map
+       });
+     }
+
+
+
+
