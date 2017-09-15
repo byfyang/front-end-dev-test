e0811fe7721c3b9c2d17c7a367c73e0412602d25
byfyang <darkspherico@gmail.com>
2017-09-15 12:00:40 -0700

    clean html and css

--- testing.js
+++ testing.js
@@ -1,10 +1,12 @@
 console.log("working");
 
-
+// scroll reveal
 window.sr = ScrollReveal();
 sr.reveal('.fadeInLeftBig', {duration: 1000, origin: 'bottom'});
 
 
+
+// google maps
 function initMap() {
        var uluru = {lat: 37.777769, lng: -122.411307};
        var map = new google.maps.Map(document.getElementById('map'), {
